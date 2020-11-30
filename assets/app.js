cf3e977b8baa98f785be3a29eaf5bf2664d1413d
Fabien Potencier <fabien@potencier.org>
2020-11-30 10:39:13 +0100

    Add 22.1: Using Sass

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
